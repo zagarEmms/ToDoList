03ae2cf10475635e601c65d2258477d3e37cd65c
emma <etarazg@gmail.com>
2022-03-10 19:08:50 +0100

    Task fragment initials

---
+++ app/src/main/java/com/example/todolist/TaskFragment.java
@@ -0,0 +1,52 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public abstract class TaskFragment extends Fragment {
+    private Task task;
+    private EditText titleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_task, container, false);
+
+        titleField = (EditText) v.findViewById(R.id.add_name);
+        titleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                titleField.setText(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        return v;
+    }
+}
