99f89c5efb6af0f342a63f25d709f3bd791578e4
emma <etarazg@gmail.com>
2022-03-12 17:12:52 +0100

    Task holder xml and java

---
+++ app/src/main/java/com/example/todolist/TaskHolder.java
@@ -0,0 +1,41 @@
+package com.example.todolist;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    private Task taskItem;
+    private TextView task_tile;
+    private ImageButton checkButton;
+
+    public TaskHolder(LayoutInflater inflater, ViewGroup parent) {
+        super(inflater.inflate(R.layout.list_item_task, parent, false));
+        itemView.setOnClickListener(this);
+
+        task_tile = (TextView) itemView.findViewById(R.id.task_title);
+        checkButton = (ImageButton) itemView.findViewById(R.id.checkTask);
+
+    }
+
+    public void bind (Task task) {
+        taskItem = task;
+        task_tile.setText(taskItem.getTaskTitle());
+        if (taskItem.isTaskDone()) {
+            checkButton.setImageResource(R.drawable.check);
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        taskItem.setTaskDone();
+    }
+
+
+}
