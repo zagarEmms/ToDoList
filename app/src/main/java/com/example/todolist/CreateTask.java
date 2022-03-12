98da94acc5df53f70775b30b7ed923d1dcd4f1e9
superfer2002 <fporcel2002@gmail.com>
2022-03-12 19:10:45 +0100

    createTask() button, changeActivity and fragment declaration.

--- app/src/main/java/com/example/todolist/CreateTask.java
+++ app/src/main/java/com/example/todolist/CreateTask.java
@@ -1,25 +1,61 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class CreateTask extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-    /*private void changeActivity () {
+public abstract class CreateTask extends AppCompatActivity {
 
-        Intent intent = new Intent(this, CreateTask.class);
+    private ExtendedFloatingActionButton addButton;
+    private String taskTitle;
 
-        intent.putExtra("activityNumber", infoArrayList);
+    private void changeActivity () {
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        intent.putExtra("taskTitle", taskTitle);
         startActivity(intent);
-        infoArrayList.clear();
 
-    }*/
+    }
+
+    private void setButton () {
+
+        addButton = (ExtendedFloatingActionButton) addButton.findViewById(R.id.add_task);
+        addButton.setOnClickListener(new View.OnClickListener()
+             {
+                 @Override
+                 public void onClick(View view) {
+                     changeActivity();
+                 }
+             }
+        );
+    }
+
+    protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainerView);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerView, fragment)
+                .commit();
+        }
+
+        setButton();
+
     }
 }
