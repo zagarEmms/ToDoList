16beb369b408e2158457c0cfbc21386ec07c0347
superfer2002 <fporcel2002@gmail.com>
2022-03-12 17:22:47 +0100

    Button intent add task.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Task> taskArrayList = new ArrayList<Task>();
+    private FloatingActionButton addButton;
+    private ListView list;
 
     private void fillTasks () {
 
@@ -28,9 +32,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void setButton () {
+
+        addButton.setOnClickListener(new View.OnClickListener()
+             {
+                @Override
+                public void onClick(View view) {
+                    changeActivity();
+                }
+            }
+        );
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fillTasks();
+        setButton();
     }
 }
